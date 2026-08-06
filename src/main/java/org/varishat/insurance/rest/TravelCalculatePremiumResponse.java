3bca6113c3b6e752e1f5c7c39ab2de4eedb752ba
Rishat2 <risch4tv@yandex.ru>
2026-08-06 22:23:44 +0300

    feat (rest): add TravelCalculatePremiumResponse class

---
+++ src/main/java/org/varishat/insurance/rest/TravelCalculatePremiumResponse.java
@@ -0,0 +1,13 @@
+package org.varishat.insurance.rest;
+
+import java.util.Date;
+
+public class TravelCalculatePremiumResponse {
+
+    private String personFirstName;
+    private String personLastName;
+    private Date agreementDateFrom;
+    private Date agreementDateTo;
+
+
+}
