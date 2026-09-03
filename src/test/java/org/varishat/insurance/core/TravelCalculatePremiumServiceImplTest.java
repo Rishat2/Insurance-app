5e53842269cb8527936015bd3b56097a987e38a4
Rishat2 <risch4tv@yandex.ru>
2026-09-03 14:07:35 +0300

    refactor(tests): split TravelCalculatePremiumServiceImplTest into atomic scenarios

--- src/test/java/org/varishat/insurance/core/TravelCalculatePremiumServiceImplTest.java
+++ src/test/java/org/varishat/insurance/core/TravelCalculatePremiumServiceImplTest.java
@@ -13,18 +13,34 @@ public class TravelCalculatePremiumServiceImplTest {
     private final TravelCalculatePremiumServiceImpl service = new TravelCalculatePremiumServiceImpl();
 
     @Test
-    public void shouldCompletedResponse() {
+    public void shouldFillPersonFirstName() {
         TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest();
         request.setPersonFirstName("Ivan");
-        request.setPersonLastName("Ivanov");
-        request.setAgreementDateFrom(new Date());
-        request.setAgreementDateTo((new Date()));
-
         TravelCalculatePremiumResponse response = service.calculatePremium(request);
-
         assertEquals(request.getPersonFirstName(), response.getPersonFirstName());
+    }
+
+    @Test
+    public void shouldFillPersonLastName() {
+        TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest();
+        request.setPersonLastName("Ivanov");
+        TravelCalculatePremiumResponse response = service.calculatePremium(request);
         assertEquals(request.getPersonLastName(), response.getPersonLastName());
+    }
+
+    @Test
+    public void shouldFillAgreementDateFrom() {
+        TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest();
+        request.setAgreementDateFrom(new Date());
+        TravelCalculatePremiumResponse response = service.calculatePremium(request);
         assertEquals(request.getAgreementDateFrom(), response.getAgreementDateFrom());
+    }
+
+    @Test
+    public void shouldFillAgreementDateTo() {
+        TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest();
+        request.setAgreementDateTo(new Date());
+        TravelCalculatePremiumResponse response = service.calculatePremium(request);
         assertEquals(request.getAgreementDateTo(), response.getAgreementDateTo());
     }
 }
