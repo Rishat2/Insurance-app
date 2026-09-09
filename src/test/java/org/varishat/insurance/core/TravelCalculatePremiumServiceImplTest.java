2777717021c4a9e47c8ddf72bb6f6735e2fad74f
Rishat2 <risch4tv@yandex.ru>
2026-09-09 22:24:00 +0300

    test (core): add mocks for TravelCalculatePremiumServiceImplTest

--- src/test/java/org/varishat/insurance/core/TravelCalculatePremiumServiceImplTest.java
+++ src/test/java/org/varishat/insurance/core/TravelCalculatePremiumServiceImplTest.java
@@ -2,54 +2,58 @@ package org.varishat.insurance.core;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.varishat.insurance.rest.TravelCalculatePremiumRequest;
 import org.varishat.insurance.rest.TravelCalculatePremiumResponse;
 
 import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 public class TravelCalculatePremiumServiceImplTest {
 
     private TravelCalculatePremiumServiceImpl service;
 
+    private TravelCalculatePremiumRequest request;
+
     @BeforeEach
     public void setUp() {
-        DateTimeService timeService = new DateTimeService();
+        request = createRequestWithAllField();
+        DateTimeService timeService = mock(DateTimeService.class);
+        when(timeService.getDiffOfDays(request.getAgreementDateFrom(), request.getAgreementDateTo())).thenReturn(0L);
         service = new TravelCalculatePremiumServiceImpl(timeService);
     }
 
     @Test
     public void shouldFillPersonFirstName() {
-        TravelCalculatePremiumRequest request = createRequestWithAllField();
         TravelCalculatePremiumResponse response = service.calculatePremium(request);
         assertEquals(request.getPersonFirstName(), response.getPersonFirstName());
     }
 
     @Test
     public void shouldFillPersonLastName() {
-        TravelCalculatePremiumRequest request = createRequestWithAllField();
         TravelCalculatePremiumResponse response = service.calculatePremium(request);
         assertEquals(request.getPersonLastName(), response.getPersonLastName());
     }
 
     @Test
     public void shouldFillAgreementDateFrom() {
-        TravelCalculatePremiumRequest request = createRequestWithAllField();
         TravelCalculatePremiumResponse response = service.calculatePremium(request);
         assertEquals(request.getAgreementDateFrom(), response.getAgreementDateFrom());
     }
 
     @Test
     public void shouldFillAgreementDateTo() {
-        TravelCalculatePremiumRequest request = createRequestWithAllField();
         TravelCalculatePremiumResponse response = service.calculatePremium(request);
         assertEquals(request.getAgreementDateTo(), response.getAgreementDateTo());
     }
 
     @Test
     public void shouldFillAgreementPrice() {
-        TravelCalculatePremiumRequest request = createRequestWithAllField();
         TravelCalculatePremiumResponse response = service.calculatePremium(request);
         assertNotNull(response.getAgreementPrice());
     }
